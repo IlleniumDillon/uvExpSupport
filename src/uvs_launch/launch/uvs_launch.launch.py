2fc3fdaf5d5baa379213bc3fe4eee055376fff76
IlleniumDillon <1476900130@qq.com>
2024-10-24 21:28:52 +0800

    Refactor uvs_launch.launch.py to add launch nodes for uvs_optitrack and uvs_embmaster

--- src/uvs_launch/launch/uvs_launch.launch.py
+++ src/uvs_launch/launch/uvs_launch.launch.py
@@ -21,8 +21,38 @@ def generate_launch_description():
     package_name = 'uvs_launch'
     package_share_dir = get_package_share_directory(package_name)
     
+    LocalIPv4Addr = DeclareLaunchArgument(
+        "LocalIPv4Addr",default_value=TextSubstitution(text='192.168.50.101')
+    )
+    ServerIPv4Addr = DeclareLaunchArgument(
+        "ServerIPv4Addr",default_value=TextSubstitution(text='192.168.50.194')
+    )
+    
     ld = LaunchDescription()
     
     ### TODO: Add the launch node
+    ld.add_action(LocalIPv4Addr)
+    
+    ld.add_action(ServerIPv4Addr)
+    
+    ld.add_action(
+        Node(
+            package="uvs_optitrack",
+            executable="uvs_optitrack",
+            output='screen',
+            parameters=[{
+                "LocalIPv4Addr": LaunchConfiguration("LocalIPv4Addr"),
+                "ServerIPv4Addr": LaunchConfiguration("ServerIPv4Addr")
+            }]
+        )
+    )
+    
+    ld.add_action(
+        Node(
+            package='uvs_embmaster',
+            executable='uvs_embmaster',
+            output='screen'
+        )
+    )
     
     return ld
